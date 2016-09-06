4935d0980e77844c285b5a78a81efbab17e6c181
mli2830 <wzmichael.li@gmail.com>
2016-09-06 09:40:08 -0400

    getting rid of all manual parameters in fit to parameter file

--- fit.R
+++ fit.R
@@ -13,8 +13,8 @@ if(observation == "nb"){
 }
 
 if(observation == "bb"){
-  niminits <- lme4:::namedList(I=sim$I,effprop,R0,repprop,N0,repobsa=0.1,repobsb=0.1)
-  nimcon <- lme4:::namedList(numobs ,N ,i0,repobsSize=10)
+  niminits <- lme4:::namedList(I=sim$I,effprop,R0,repprop,N0,repobsa,repobsb)
+  nimcon <- lme4:::namedList(numobs ,N ,i0,repobsSize)
   }
 
 params <- c("R0","effprop","repprop")
@@ -25,7 +25,7 @@ stanmod <- ""
 if(type=="hyb"){
   niminits <- lme4:::namedList(I=sim$I*repprop,effprop,R0,repprop, N0,repShape=0.1)
   if(observation == "nb"){
-    niminits <- lme4:::namedList(I=sim$I*repprop,obsMean=sim$I*repprop,effprop,R0,repprop,N0, repShape=0.1)
+    niminits <- lme4:::namedList(I=sim$I*repprop,obsMean=sim$I*repprop,effprop,R0,repprop,N0, repShape)
   }
   mcmcs <- c("jags"
                ,"nimble"
--- parameters.CBB.R
+++ parameters.CBB.R
@@ -1,13 +1,20 @@
+# real parameters
 R0 <- 2
-N <- 10000
-i0 <- 1
 effprop <- 0.9
 repprop <- 0.5
-repSize <- 1000
 
-eps <-0.0001
+#bb observation parameters 
+repobsa <- 0.1
+repobsb <- 0.1
+
+
+# init values and constants
+N <- 10000
+i0 <- 1
+repSize <- 1000
 initDis <- 0.1
 N0 <- round(N*effprop)
 numobs <- 15
-
+repobsSize <- 10
+repShape <- 0.1
 Ndis=2
