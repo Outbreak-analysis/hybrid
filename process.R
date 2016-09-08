9fb2ed09a77ed13353d331b37bd452663a09d278
mli2830 <wzmichael.li@gmail.com>
2016-09-07 23:52:30 -0400

    forgot to update process

--- process.R
+++ process.R
@@ -70,3 +70,71 @@ if(process == "bb"){
     )
   }
 }
+
+## Poisson Process ----
+if(process == "p"){
+  if(type == "dis"){
+    process_code <- c("
+                      I[1] ~ dpois(i0)
+                      beta <- R0/N0
+                      pSI[1] <- 1 - exp(-I[1]*beta)
+                      "
+                      ,"
+                      I[t] ~ dpois(pSI[t-1]*S[t-1])
+                      pSI[t] <- 1 - exp(-I[t]*beta)
+                      "
+    )
+  }
+  if(type == "hyb"){
+    process_code <- c("
+                      I[1] ~ dgamma(i0,1/repprop)
+                      beta <- R0/N0
+                      pSI[1] <- 1 - exp(-I[1]*beta)
+                      SIGrate[1] <- 1
+                      SIGshape[1] <- 1
+                      "
+      ,"
+      SIGrate[t] <- 1
+      SIGshape[t] <- pSI[t-1]*SIGrate[t]*(S[t-1]/repprop)
+      I[t] ~ dgamma(SIGshape[t],SIGrate[t])
+      pSI[t] <- 1 - exp(-I[t]*beta)
+      "
+    )
+  }
+}
+
+## Poisson Process ----
+if(process == "nb"){
+  if(type == "dis"){
+    process_code <- c("
+                      pSISize ~ dgamma(1,1)
+                      Imean[1] ~ dgamma(pSISize,pSISize/i0)
+                      I[1] ~ dpois(Imean[1])
+                      beta <- R0/N0
+                      pSI[1] <- 1 - exp(-I[1]*beta)
+                      "
+                      ,"
+                      Imean[t] ~ dgamma(pSISize,pSISize/(S[t-1]*pSI[t-1]))
+                      I[t] ~ dpois(Imean[t])
+                      pSI[t] <- 1 - exp(-I[t]*beta)
+                      "
+    )
+  }
+  if(type == "hyb"){
+    process_code <- c("
+                      pSISize ~ dgamma(1,1)
+                      I[1] ~ dgamma(i0,1/repprop)
+                      beta <- R0/N0
+                      pSI[1] <- 1 - exp(-I[1]*beta)
+                      SIGrate[1] <- 0.1
+                      SIGshape[1] <- 0.1
+                      "
+      ,"
+      SIGrate[t] <- pSISize/(S[t-1]*pSI[t-1]/repprop)
+      SIGshape[t] <- pSI[t-1]*SIGrate[t]*(S[t-1]/repprop)
+      I[t] ~ dgamma(SIGshape[t],SIGrate[t])
+      pSI[t] <- 1 - exp(-I[t]*beta)
+      "
+    )
+  }
+}
